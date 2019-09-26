4dc95171d6e813c6247fc943446082da6729f1ab
Marcos <marcos.camberto@aluno.sc.senac.br>
2019-09-26 01:04:59 -0300

    ajuste

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:cadastro_app/ui/login.dart';
 import 'package:cadastro_app/ui/home.dart';
 import 'package:cadastro_app/helper/login_helper.dart';
 
-void main() {
+void main() async {
   LoginHelper helper = LoginHelper();
   runApp(MaterialApp(
-    home: helper.session == true ? HomePage() : LoginPage(),
+    home: (await helper.session == true) ? HomePage() : LoginPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
